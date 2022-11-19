4f095752a9ecf50ab7445bf2de877a94b9856645
New User <newuser@News-MacBook-Air.local>
2022-11-19 10:58:00 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,59 @@
+function changeImg(){
+    const sImage = document.querySelector(".sni-img");
+    const sHead = document.querySelector(".head");
+    const sPara = document.querySelector(".para");
+    const sPrice = document.querySelector(".price");
+
+    sImage.src = "/imagesdom2/pink-canvas-sneakers-with-polka-dot-unisex-footwear-fashion_53876-106039.webp";
+    sHead.innerHTML = "Pink sneakers";
+    sPara.innerHTML = "comes in different shapes and sizes";
+    sPrice.innerHTML = "$70";
+}
+
+function changeImg1(){
+    const sImage = document.querySelector(".sni-img");
+    const sHead = document.querySelector(".head");
+    const sPara = document.querySelector(".para");
+    const sPrice = document.querySelector(".price");
+
+    sImage.src = "/imagesdom2/sneakers.webp";
+    sHead.innerHTML = "Yellow Converse";
+    sPara.innerHTML = "Available for females, comes only in color displayed";
+    sPrice.innerHTML = "$60";
+}
+
+function changeImg2(){
+    const sImage = document.querySelector(".sni-img");
+    const sHead = document.querySelector(".head");
+    const sPara = document.querySelector(".para");
+    const sPrice = document.querySelector(".price");
+
+    sImage.src = "/imagesdom2/how-to-clean-white-sneakers-converse.jpeg";
+    sHead.innerHTML = "White Sneakers";
+    sPara.innerHTML = "Available for females, sizes 40 - 45";
+    sPrice.innerHTML = "$100";
+}
+
+function changeImg3(){
+    const sImage = document.querySelector(".sni-img");
+    const sHead = document.querySelector(".head");
+    const sPara = document.querySelector(".para");
+    const sPrice = document.querySelector(".price");
+
+    sImage.src = "/imagesdom2/photo-1607522370275-f14206abe5d3.jpeg";
+    sHead.innerHTML = "Red and White converse";
+    sPara.innerHTML = "Available for females, sizes 40 - 45";
+    sPrice.innerHTML = "$120";
+}
+
+function changeImg4(){
+    const sImage = document.querySelector(".sni-img");
+    const sHead = document.querySelector(".head");
+    const sPara = document.querySelector(".para");
+    const sPrice = document.querySelector(".price");
+
+    sImage.src = "/imagesdom2/606_980-9908FF08-0FE9-407B-B79F-B8FEA4D6CD12.jpeg";
+    sHead.innerHTML = "Blue and White Nike";
+    sPara.innerHTML = "comes in sizes 39 - 46";
+    sPrice.innerHTML = "$150";
+}
